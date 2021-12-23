24ac696204b765d99d175ee39f5914a11c138c59
honkinlan <honkinlan@163.com>
2021-12-23 18:29:44 +0800

    update 优化云识别示例

--- pages/api/cloudar/cloudar.js
+++ pages/api/cloudar/cloudar.js
@@ -1,24 +1,63 @@
-import { errorHandler, showAuthModal } from "../../../utils/utils";
+import { errorHandler, showAuthModal, requestFile } from "../../../utils/utils";
 
 Page({
   data: {
     license: getApp().globalData.license,
     showGuide: false,
-    guideTip: ""
+    guideTip: "请对准识别图"
   },
 
   onLoad() {
     wx.showLoading({ title: "初始化中...", mask: true });
+
+    this.downloadAsset = Promise.all([
+      requestFile(
+        "https://kivicube-resource.kivisense.com/wechat-kivicube-slam-plugin-api-demo/rabbit.glb"
+      )
+    ]);
   },
 
   async ready({ detail: slam }) {
-    await slam.start();
-    wx.hideLoading();
-    this.slam = slam;
-    this.setData({
-      showGuide: true,
-      guideTip: "对准识别图，点击开始识别"
-    })
+    try {
+      const [rabbitArrayBuffer] = await this.downloadAsset;
+      const [rabbitModel] = await Promise.all([
+        slam.createGltfModel(rabbitArrayBuffer),
+      ]);
+
+      // 暂时隐藏，云识别成功后再出现。
+      rabbitModel.visible = false;
+      slam.add(rabbitModel, 0.8);
+
+      await slam.start();
+
+      this.rabbitModel = rabbitModel;
+      this.slam = slam;
+
+      wx.hideLoading();
+      this.setData({
+        showGuide: true
+      })
+      const {windowHeight, windowWidth} = wx.getWindowInfo()
+
+      // 传入合辑ID进行云识别
+      this.slam.startCloudAr("b46rfc").then(sceneId => {
+        // 识别成功，显示模型
+        if(sceneId) {
+          this.setData({ showGuide: false })
+          this.setModel(windowWidth / 2, windowHeight / 2)
+        }
+      });
+
+      // 超过10秒未识别到，关闭云识别，直接显示模型
+      setTimeout(() => {
+        this.slam.stopCloudAr()
+        this.setData({ showGuide: false })
+        this.setModel(windowWidth / 2, windowHeight / 2)
+      }, 10000)
+    } catch (e) {
+      wx.hideLoading();
+      errorHandler(e);
+    }
   },
 
   error({ detail }) {
@@ -30,34 +69,30 @@ Page({
     }
   },
 
-  async handleStart() {
-    this.setData({
-      showGuide: true,
-      guideTip: "请对准识别图"
-    })
-
-    const sceneId = await this.slam.startCloudAr("b46rfc")
-    if(!sceneId){
-      return
+  tap({ touches, target }) {
+    if (Array.isArray(touches) && touches.length > 0) {
+      const { offsetLeft, offsetTop } = target;
+      const { pageX, pageY } = touches[0];
+      // 注意：需要传入在kivicube-slam组件上的坐标点，而不是页面上的坐标点。
+      const x = pageX - offsetLeft;
+      const y = pageY - offsetTop;
+      this.setModel(x, y)
     }
-
-    wx.showModal({
-      title: "提示",
-      content: `${sceneId}`,
-      showCancel: false
-    });
-    
-    this.setData({
-      showGuide: false
-    })
   },
 
-  handleStop() {
-    this.slam.stopCloudAr()
-    
-    this.setData({
-      showGuide: false
-    })
+  setModel(x, y) {
+    const success = this.slam.standOnThePlane(
+      this.rabbitModel,
+      x,
+      y,
+      true
+    );
+    if (success) {
+      this.rabbitModel.visible = true;
+      this.rabbitModel.playAnimation({ loop: true });
+    } else {
+      wx.showToast({ title: "放置模型失败，请对准平面", icon: "none" });
+    }
   }
 
 });
