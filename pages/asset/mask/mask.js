26adce7a48d07969960e7744189637f6168b120f
宏金 蓝 <honkinlan@163.com>
2023-03-13 17:08:08 +0800

    update: 遮罩示例优化

--- pages/asset/mask/mask.js
+++ pages/asset/mask/mask.js
@@ -13,44 +13,52 @@ Page({
         "https://meta.kivisense.com/kivicube-slam-mp-plugin/demo-assets/model/rabbit.glb"
       ),
       requestFile(
-        "https://meta.kivisense.com/kivicube-slam-mp-plugin/demo-assets/model/robot.glb"
+        "https://meta.kivisense.com/kivicube-slam-mp-plugin/demo-assets/model/mask.glb"
       ),
     ]);
   },
 
   async ready({ detail: slam }) {
     try {
-      const [rabbitArrayBuffer, robotArrayBuffer] = await this.downloadAsset;
-      const [rabbit, robot] = await Promise.all([
+      const [rabbitArrayBuffer, maskArrayBuffer] = await this.downloadAsset;
+      const [rabbit, mask] = await Promise.all([
         slam.createGltfModel(rabbitArrayBuffer),
-        slam.createGltfModel(robotArrayBuffer),
+        slam.createGltfModel(maskArrayBuffer),
       ]);
 
       // 启用遮罩效果。遮罩可以将遮罩自身，和遮罩后方的所有内容，全部置为纯透明。
       // 达到去除特定形状的目的，以模拟真实物体遮挡等功能。
-      robot.setEnableMask();
+      mask.setEnableMask();
       // 禁用遮罩效果
       // robot.setDisableMask();
 
-      robot.scale.setScalar(0.01);
-      robot.position.z = 0.005;
-      rabbit.add(robot);
+      // 创建一个组合对象
+      const group = slam.createGroup();
+      group.visible = false;
 
-      slam.add(rabbit, 0.5);
+      // 向组合对象分别加入遮罩模型和需要被遮罩遮住的模型
+      group.add(mask);
+      group.add(rabbit);
+
+      // 让兔子模型往下移动一点距离, 进入桶状的遮罩模型的内部
+      rabbit.position.y = -0.2;
+      
+      // 加入组合对象
+      slam.add(group, 1);
 
       rabbit.playAnimation({ loop: true });
-      robot.playAnimation({ loop: true });
 
       // 开启slam平面追踪
       await slam.start();
 
       const { windowWidth, windowHeight } = wx.getSystemInfoSync();
       const success = slam.standOnThePlane(
-        rabbit,
+        group,
         Math.round(windowWidth / 2),
         Math.round(windowHeight / 2),
         true
       );
+      group.visible = true;
       console.log("standOnThePlane success", success);
 
       wx.hideLoading();
